9028cb1da2382da41371917934a625e6a571510d
LongChauThien <long.chauthien03@gmail.com>
2024-11-16 09:42:47 +0700

    Add database for project

---
+++ data/Crawl/taxi/service.php
@@ -0,0 +1,31 @@
+<?php
+
+// Connect to the database.
+$mysqli = new mysqli("localhost", "root", "", "db_ie104");
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+  } else {
+    echo "Connected successfully" . "<br>";
+  }
+
+// // Read the JSON file.
+$json_file = file_get_contents("./Data/service.json");
+
+// Decode the JSON file into an array.
+$json_array = json_decode($json_file, true);
+
+
+// Loop through the JSON array and insert each row into the table.
+foreach ($json_array as $row) {
+  $sql = "INSERT INTO service VALUES ('".$row['Id']."','".$row['Name']."')";
+  if ($mysqli->query($sql) === TRUE) {
+    echo "Record inserted successfully";} 
+    else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+
+// Close the database connection.
+$mysqli->close();
+
+?>
